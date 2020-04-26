8cf0af2bbcda265fb57f178bd4853e5bf5edff98
Adam <adam.lababidi@gmx.com>
2020-04-26 15:44:57 +0200

    Cleanup

--- samples/slimWebhook.php
+++ samples/slimWebhook.php
@@ -5,7 +5,18 @@ $app = new App();
 
 // https://yourdomain.com/webhookExampleLink
 $app->post('/webhookExampleLink', function(Request $request, Response $response, array $args){
-    list($txid,$confirmations,$address, $amount, $currency, $tcn, $webhookSecret) = $request->getParams();
+
+    $txid = $request->getParam('txid');
+    $confirmations = $request->getParam('confirmations');
+    $address = $request->getParam('address');
+    $amount = $request->getParam('amount');
+    $currency = $request->getParam('currency');
+    $tcn = $request->getParam('tcn');
+    $webhookSecret = $request->getParam('webhookSecret');
+
+    if ($webhookSecret !== 'YOUR_IBITCOIN_SECRET_FROM_YOUR_ACCOUNT') {
+        return $response->getBody()->write('{"success":false}');
+    }
 
     if ($confirmations === 0){
         // e.g. add transaction to database
